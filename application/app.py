42290824796fe9ee578cae15804205669cc26df7
Jing Z <zhang.jing.1997@outlook.com>
2024-10-28 11:37:01 +0100

    Update app.py

--- application/app.py
+++ application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
@@ -20,15 +20,17 @@ def init_db():
                    User_ID INTEGER NOT NULL, 
                    Post_title TEXT NOT NULL,
                    Description TEXT,
+                   Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                    FOREIGN KEY(User_ID) REFERENCES users(User_ID)
                    )                  
 ''')      
     cursor.execute('''
             CREATE TABLE IF NOT EXISTS comments (
                    Comment_ID AUTO_INCREMENT PRIMARY KEY,
-                   USER_ID INTEGER NOT NULL, 
+                   User_ID INTEGER NOT NULL, 
                    Post_ID INTEGER NOT NULL,
                    Description TEXT NOT NULL,
+                   Created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                    FOREIGN KEY(Post_ID) REFERENCES posts(Post_ID)
                    FOREIGN KEY(User_ID) REFERENCES users(User_ID)
                    )                  
@@ -38,5 +40,61 @@ def init_db():
 
 init_db()
 
-@app.route('')
+@app.route('/', methods=['GET'])
+def home():
+    con = sqlite3.connect('blogs.db')
+    cur = con.cursor()
+    posts = cur.execute('''SELECT Post_title, User_ID, Description, Created_at FROM posts 
+                        JOIN users ON posts.User_ID = users.User_ID''').fetchall()
+    con.close()
+    return jsonify(dict(posts)), 200
 
+@app.route('/post', methods=['POST'])
+def add_post():
+    data = request.get_json()
+    title = data.get('Post_title')
+    description = data.get('Description')
+
+    con = sqlite3.connect('blogs.db')
+    cur = con.cursor()
+    cur.execute('INSERT INTO posts (Post_title, Description) VALUES (?, ?)', (title, description))
+    con.commit()
+    con.close()
+
+    return jsonify({'Message': 'Your post has been created successfully.'}), 201
+
+@app.route('/post', methods=['DELETE'])
+def delete_post(post_id):
+    data = request.get_json()
+    post_id = data.get('Post-ID')
+
+    con = sqlite3.connect('blogs.db')
+    cur = con.cursor()
+    cur.execute('DELETE FROM posts WHERE Post-ID = (?)', (post_id))
+    
+    return jsonify({'Message': 'Your post has been delected.'}), 200
+
+@app.route('/comment', methods=['POST'])
+def add_post():
+    data = request.get_json()
+    title = data.get('Post_title')
+    description = data.get('Description')
+
+    con = sqlite3.connect('blogs.db')
+    cur = con.cursor()
+    cur.execute('INSERT INTO posts (Post_title, Description) VALUES (?, ?)', (title, description))
+    con.commit()
+    con.close()
+
+    return jsonify({'Message': 'Your post has been created successfully.'}), 201
+
+@app.route('/comment', methods=['DELETE'])
+def delete_post():
+    data = request.get_json()
+    post_id = data.get('Post-ID')
+
+    con = sqlite3.connect('blogs.db')
+    cur = con.cursor()
+    cur.execute('DELETE FROM posts WHERE Post-ID = (?)', (post_id))
+    
+    return jsonify({'Message': 'Your post has been delected.'}), 200
