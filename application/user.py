39a5de4d25379089c165bc7dbdd2a09b6fbb1afa
AmyPap <amypapakosta@gmail.com>
2024-10-31 21:58:38 +0100

    Change to with sqlite3.connect as con

--- application/user.py
+++ application/user.py
@@ -10,21 +10,18 @@ def register():
     username = data.get('Username')
     password = data.get('Password')
 
-    con = sqlite3.connect('blogs.db')
-    cur = con.cursor()
+    with sqlite3.connect('blogs.db') as con:
+        cur = con.cursor()
 
     # Insert new user if username doesn't already exists
-    try:
-        cur.execute('INSERT INTO users (Username, Password, Logged_in) VALUES (?, ?, ?)', (username, password, 0))
-        con.commit()
-        con.close()
-        return jsonify({'Message': 'User successfully created.'}), 201
-    except sqlite3.OperationalError:
-        con.close()
-        return jsonify({'Error': 'Username already exists.'}), 400
-    except sqlite3.IntegrityError:
-        con.close()
-        return jsonify({'Error': 'You need to give a valid "Username" and "Password".'})
+        try:
+            cur.execute('INSERT INTO users (Username, Password, Logged_in) VALUES (?, ?, ?)', (username, password, 0))
+            con.commit()
+            return jsonify({'Message': 'User successfully created.'}), 201
+        except sqlite3.OperationalError:
+            return jsonify({'Error': 'Username already exists.'}), 400
+        except sqlite3.IntegrityError:
+            return jsonify({'Error': 'You need to give a valid "Username" and "Password".'})
 
 # Login 
 
@@ -34,44 +31,40 @@ def login():
     username = data.get('Username')
     password = data.get('Password')
 
-    con = sqlite3.connect('blogs.db')
-    cur = con.cursor()
-
+    with sqlite3.connect('blogs.db') as con:
+        cur = con.cursor()
     # Find if username, password match a specific user
-    cur.execute('''SELECT Username, Password FROM users 
+        cur.execute('''SELECT Username, Password FROM users 
                 Where Username = ? AND Password = ?''', (username, password))
 
     # If exists, change loging status in db
-    if cur.fetchone():
-        cur.execute('''UPDATE users 
-                    SET Logged_in = 1
-                    Where Username = ?''', (username,))
-        con.commit()
-        con.close()
-        return jsonify({'Message': 'Welcome! You are now logged in.'}), 200
-    else:
-        con.close()
-        return jsonify({'Error': 'Wrong credentials.'}), 404
+        if cur.fetchone():
+            cur.execute('''UPDATE users 
+                        SET Logged_in = 1
+                        Where Username = ?''', (username,))
+            con.commit()
+            return jsonify({'Message': 'Welcome! You are now logged in.'}), 200
+        else:
+            return jsonify({'Error': 'Wrong credentials.'}), 404
     
 def logout():
     # Save received json data in variables
     data = request.get_json()
     username = data.get('Username')
 
-    con = sqlite3.connect('blogs.db')
-    cur = con.cursor()
+    with sqlite3.connect('blogs.db') as con:
+        cur = con.cursor()
 
     # Check if user is logged in
-    cur.execute('''SELECT Logged_in FROM users 
-                Where Username = ?''', (username,))
+        cur.execute('''SELECT Logged_in FROM users 
+                 Where Username = ?''', (username,))
     
     # If match is a tuple (1,0) which means logged in
-    if cur.fetchone() == (1,):
-        cur.execute('''UPDATE users 
-                    SET Logged_in = 0
-                    Where Username = ?''', (username,))
-        con.commit()
-        con.close()
-        return jsonify({'Message': 'You are now logged out.'}), 200
-    else: 
-        return jsonify({'Error': 'You are not logged in.'}), 400
+        if cur.fetchone() == (1,):
+            cur.execute('''UPDATE users 
+                        SET Logged_in = 0
+                        Where Username = ?''', (username,))
+            con.commit()
+            return jsonify({'Message': 'You are now logged out.'}), 200
+        else: 
+            return jsonify({'Error': 'You are not logged in.'}), 400
