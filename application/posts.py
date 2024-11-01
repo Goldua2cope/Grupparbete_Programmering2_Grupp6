944df149cbafa674626c981503750184db9387ee
AmyPap <amypapakosta@gmail.com>
2024-11-01 16:19:52 +0100

    Update posts.py

--- application/posts.py
+++ application/posts.py
@@ -1,5 +1,7 @@
-from flask import jsonify, request
+from flask import jsonify, request , session
+from app import app
 import sqlite3
+
 
 # TODO: SKRIVA UT ALLA KOMMENTARER OCKSÅ
 
@@ -12,44 +14,42 @@ def home():
     return jsonify(posts), 200
 
 def add_post():
+    if 'username' not in session:
+        return jsonify({'Error': 'You have to be logged in to crate a post.'}), 400
+     
     data = request.get_json()
-    username = data.get('Username')
     title = data.get('Post_title')
     description = data.get('Description')
     
     with sqlite3.connect('blogs.db') as con:
         cur = con.cursor()
 
-        cur.execute('''SELECT Logged_in FROM users 
-                    Where Username = ?''', (username,))
-    
-    # If match is a tuple (1,0) which means logged in
-        if cur.fetchone() == (1,):
-            cur.execute('''INSERT INTO posts (Post_title, Description) 
+
+        cur.execute('''INSERT INTO posts (Post_title, Description) 
                         VALUES (?, ?)''', 
                         (title, description))
-            con.commit()
-            return jsonify({'Message': 'Your post has been created successfully.'}), 201
-        else: 
-            return jsonify({'Error': 'You have to be logged in to crate a post.'}), 403
+        con.commit()
+        return jsonify({'Message': 'Your post has been created successfully.'}), 201
 
 # TODO: LÄGG TILL CONDITIONS ATT ANVÄNDAREN BARA FÅ TA BORT SINA POSTS
 
 def delete_post():
+    if 'username' not in session:
+        return jsonify({'Error': 'You must be logged in to delete a post.'}) , 400
+
     data = request.get_json()
-    username = data.get('Username')
-    post_id = data.get('Post-ID')
+    post_id = data.get('Post_ID')
 
     with sqlite3.connect('blogs.db') as con:
         cur = con.cursor()
-    
-        cur.execute('''SELECT Logged_in FROM users 
-                 Where Username = ?''', (username,))
-    
-        if cur.fetchone() == (1,):
-            cur.execute('DELETE FROM posts WHERE Post-ID = (?)', (post_id))
+
+        cur.execute('SELECT Post_ID FROM posts WHERE Username = ?', (session['username'],))
+        user_result = cur.fetchone()
+
+        if user_result == None:
+            return jsonify({'Error':'Post not found'}) , 404
+        else:
+            cur.execute('DELETE FROM posts WHERE Post_ID = ?', (post_id))
             con.commit()
             return jsonify({'Message': 'Your post has been delected.'}), 200
-        else:
-            return jsonify({'Error': 'You must be logged in to delete a post.'}) , 403
-
+   
