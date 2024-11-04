e4523d117fd7b611bbad8ed36c0bcde05cc8a40d
AmyPap <amypapakosta@gmail.com>
2024-11-04 11:53:53 +0100

    Update posts.py

--- application/posts.py
+++ application/posts.py
@@ -1,55 +1,82 @@
 from flask import jsonify, request , session
-from app import app
 import sqlite3
 
-
-# TODO: SKRIVA UT ALLA KOMMENTARER OCKSÅ
-
 def home():
-    con = sqlite3.connect('blogs.db')
-    cur = con.cursor()
-    posts = cur.execute('''SELECT Post_title, User_ID, Description, Created_at FROM posts 
-                        JOIN users ON posts.User_ID = users.User_ID''').fetchall()
-    con.close()
-    return jsonify(posts), 200
+    with sqlite3.connect('blogs.db') as con: 
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        comments = cur.execute('''
+            SELECT comments.Comment_ID, comments.Comment_Description ,
+                   posts.Post_ID, posts.Post_title, posts.Post_Description
+            FROM posts
+            LEFT JOIN comments ON posts.Post_ID = comments.Post_ID
+        ''').fetchall()
+        
+        comments_list = []
+        
+        for comment in comments:
+            comment_dict = { 
+                'Comment_ID': comment['Comment_ID'],        
+                'Comment_Description': comment['Comment_Description'],
+                'Post_ID': comment['Post_ID'],
+                'Post_title': comment['Post_title'],
+                'Post_Description': comment['Post_Description']
+            }
+            comments_list.append(comment_dict)
+    
+    return jsonify(comments_list), 200
 
 def add_post():
+    print("Session contents :", dict(session))
     if 'username' not in session:
         return jsonify({'Error': 'You have to be logged in to crate a post.'}), 400
      
-    data = request.get_json()
+    data = request.get_json(silent=True)
+    if data is None:
+        return  jsonify({'Error':'Null value.'})
     title = data.get('Post_title')
-    description = data.get('Description')
+    description = data.get('Post_Description')
     
+    if not title or not description :
+        return jsonify({'Error': 'Post_title and Post_Description are required.'}), 400
+
+    user_id = session['user_id']
     with sqlite3.connect('blogs.db') as con:
         cur = con.cursor()
-
-
-        cur.execute('''INSERT INTO posts (Post_title, Description) 
-                        VALUES (?, ?)''', 
-                        (title, description))
+        #### har lagt till User_ID för att veta vem har skrivit den och bara hen kan radera den
+        cur.execute('''INSERT INTO posts ( Post_title, Post_Description, User_ID) 
+                        VALUES (?,?,?)''', 
+                        (title, description,user_id))
         con.commit()
         return jsonify({'Message': 'Your post has been created successfully.'}), 201
 
-# TODO: LÄGG TILL CONDITIONS ATT ANVÄNDAREN BARA FÅ TA BORT SINA POSTS
 
 def delete_post():
     if 'username' not in session:
         return jsonify({'Error': 'You must be logged in to delete a post.'}) , 400
 
-    data = request.get_json()
+    data = request.get_json(silent=True)
+    if data is None:
+        return  jsonify({'Error':'Null value.'})
     post_id = data.get('Post_ID')
 
+    if not post_id:
+        return jsonify({'Error': 'Post_ID is required.'}), 400
+    
     with sqlite3.connect('blogs.db') as con:
         cur = con.cursor()
+        
+        user_id = session['user_id']
 
-        cur.execute('SELECT Post_ID FROM posts WHERE Username = ?', (session['username'],))
+        cur.execute('SELECT User_ID FROM posts WHERE Post_ID = ?', (post_id,))
         user_result = cur.fetchone()
 
         if user_result == None:
             return jsonify({'Error':'Post not found'}) , 404
+        elif user_result[0] != user_id:
+            return jsonify({'Error':'You do not have permission to delete this post.'})
         else:
-            cur.execute('DELETE FROM posts WHERE Post_ID = ?', (post_id))
+            cur.execute('DELETE FROM posts WHERE Post_ID = ?', (post_id,))
             con.commit()
-            return jsonify({'Message': 'Your post has been delected.'}), 200
+            return jsonify({'Message': 'Your post has been deleted.'}), 200
    
