12202f0282318fc9f3e42b3212f4fc8f6aef3b16
SilanAslan <silanaslan@gmail.com>
2024-11-05 09:17:02 +0100

    Update user.py
    Senaste uppdateringar

--- application/user.py
+++ application/user.py
@@ -1,55 +1,64 @@
-from flask import jsonify, request , session 
-from app import app
+from flask import jsonify
 import sqlite3
 
-# Register a new user
-
-def register():
-    # Save received json data in variables
-    data = request.get_json()
-    username = data.get('Username')
-    password = data.get('Password')
-
-    with sqlite3.connect('blogs.db') as con:
-            cur = con.cursor()
-            cur.execute('SELECT * FROM users WHERE Username= ?',(username,))
-            
-            if cur.fetchone():
-                 return jsonify({'Error':'Username already exists.'})
-        
-            try:
-                cur.execute('''INSERT INTO users (Username, Password) VALUES (? , ?)''', (username, password))
-                con.commit()
-                return jsonify({'Message': 'User successfully created.'}), 201
-            except sqlite3.IntegrityError:
-                return jsonify({'Error': 'Invalid data.'}) , 400
-
-# Login 
-
-def login():
-    # Save received json data in variables
-    data = request.get_json()
-    username = data.get('Username')
-    password = data.get('Password')
-
-    with sqlite3.connect('blogs.db') as con:
+def register(username: str, password: str) -> None:
+    """
+    Registrerar en ny användare i databasen.
+    """
+    with sqlite3.connect('blogg_data.db') as con:
         cur = con.cursor()
-    # Find if username, password match a specific user
-        cur.execute('''SELECT Username, Password FROM users 
-                Where Username = ? AND Password = ?''', (username, password))
-        user = cur.fetchone()
+        cur.execute('''INSERT INTO users (Username, Password) VALUES (? , ?)''', 
+                    (username, password))
+        con.commit()
+   
+def check_credentials(username: str, password: str) -> tuple | None :
+    """
+    Kontrollerar användarens inloggningsuppgifter.
+    Returnerar en tuple med användarnamn och lösenord om de matchar, annars None.
+    """
+    with sqlite3.connect('blogg_data.db') as con:
+        cur = con.cursor()
+        return cur.execute('''SELECT Username, Password FROM users 
+                Where Username = ? AND Password = ?''', 
+                (username, password)).fetchone()
+
+def check_status(user_id: int) -> int:
+    """
+    Kontrollerar användarens status.
+    Returnerar användarens statusvärde.
+    """
+    with sqlite3.connect('blogg_data.db') as con:
+        cur = con.cursor()
+        user_status = cur.execute('SELECT Status FROM users WHERE User_ID = ?', 
+                           (user_id,)).fetchone()
+        return user_status[0]
+
+def get_user_id(username: str) -> int:
+    """
+    Hämtar användar-ID baserat på användarnamn.
+    Returnerar användarens ID.
+    """
+    with sqlite3.connect('blogg_data.db') as con:
+        cur = con.cursor()
+        user_id = cur.execute('SELECT User_ID from users WHERE Username = ?', 
+                           (username,)).fetchone()
+        return user_id[0]
+
+def change_status(user_id: int, value: int) -> None:
+    """ Ändrar användarens status i databasen."""
     
-    # check if user exists
-        if user:
-            session['username'] = username
-            return jsonify({'Message': 'Welcome! You are now logged in.'}), 200
-        else:
-            return jsonify({'Error': 'Wrong credentials.'}), 404
+    with sqlite3.connect('blogg_data.db') as con: 
+        cur = con.cursor()
+        cur.execute('UPDATE users SET Status = ? WHERE User_ID = ?',
+                    (value, user_id))
+        con.commit()
+
+def check_login(session: dict) -> bool:
+    """
+    Kontrollerar om användaren är inloggad.
+    Returnerar True om användaren är inloggad, annars False.
+    """
     
-def logout():
-    # Save received json data in variables
-    if 'username' in session:
-        session.pop('username', None) 
-        return jsonify({'Message': 'You are now logged out.'}), 200
-    else: 
-        return jsonify({'Error': 'You are not logged in.'}), 400
+    if 'username' in session and check_status(session.get("user_id", 0)) == 1:
+        return True
+    return False
